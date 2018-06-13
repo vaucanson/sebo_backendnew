0ec76f7fea4fcef02e95be5e2450c7a56335846b
BadarouxY <badaroux.yannick@gmail.com>
2018-06-13 13:52:02 +0200

    maj yann

--- src/java/entity/ClientOrder.java
+++ src/java/entity/ClientOrder.java
@@ -166,10 +166,5 @@ public class ClientOrder implements Serializable {
     public String toString() {
         return "ClientOrder{" + "id=" + id + ", orderState=" + orderState + ", address=" + address + ", client=" + client + ", orderDate=" + orderDate + ", paymet=" + paymet + ", deliveryDate=" + deliveryDate + '}';
     }
-    
-<<<<<<< HEAD
-    
 
-=======
->>>>>>> master
 }
